31b4b3bc6700bf822f58208f7e89e6d1af8d46fb
Gabby Nunez <gabbynunez@Gabbys-MacBook-Pro.local>
2023-04-06 16:26:14 -0500

    middleware has been created

--- api/server.js
+++ api/server.js
@@ -7,4 +7,10 @@ const server = express();
 server.use(express.json());
 server.use('/api/schemes', SchemeRouter);
 
+server.use('*', (req, res, next) => {
+  next({ status: 404, message: 'not found'})
+})
+
+
+
 module.exports = server;
